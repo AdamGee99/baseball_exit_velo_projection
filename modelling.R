0a36940e2698c7c2638600c85bcad910ec984f6f
AdamGee99 <arg15@sfu.ca>
2026-01-14 11:18:17 -0800

    cenetering heights instead of scaling

--- modelling.R
+++ modelling.R
@@ -12,17 +12,26 @@ library(scattermore)
 ############################################## Modelling ############################################## 
 
 mlb_full = read.csv(here("data", "mlb_2024_2025.csv"))
+#mapping of batter ids to stan batter ids
+id_map = mlb_full %>% select(batter, stan_batter_id) %>% distinct()
+
+
+#need to scale heights and weights for identifiability issues
+mlb_full = mlb_full %>% mutate(height_centered = (height - mean(height)),
+                               weight_centered = (weight - mean(weight)))
+
 
 #train on 2024, test on 2025
 train = mlb_full %>% filter(game_year == 2024)
 test = mlb_full %>% filter(game_year == 2025)
 
-#true player means in 2025 season - target
+#true player means in 2025 season (target)
 true_vals = test %>%
   select(stan_batter_id, exit_velo) %>%
   group_by(stan_batter_id) %>%
   summarise(true_mean_exit_velo = mean(exit_velo)) %>%
   ungroup()
+
 
 
 
@@ -34,19 +43,21 @@ train_subset = train %>% filter(stan_batter_id %in% 1:10)
 stan_data_subset = list(N = nrow(train_subset),
                         J = train_subset$stan_batter_id %>% unique() %>% length(),
                         y = train_subset$exit_velo,
-                        id = train_subset$stan_batter_id)
+                        id = train_subset$stan_batter_id,
+                        height = train_subset$height_centered)
 #input to stan
 stan_data = list(N = nrow(train),
                  J = train$stan_batter_id %>% unique() %>% length(),
                  y = train$exit_velo,
-                 id = train$stan_batter_id)
+                 id = train$stan_batter_id,
+                 height = train$height_centered)
 
 stan_file = here("stan", "baseline.stan")
 #model
 mod = cmdstan_model(stan_file)
 
 #sample from model
-fit = mod$sample(data = stan_data,
+fit = mod$sample(data = stan_data_subset,
                  seed = 123,
                  chains = 4,
                  parallel_chains = 4,
@@ -64,8 +75,8 @@ fit$summary()
 #all chains show good convergence - rhat close to 1
 
 #posteriors
-mcmc_areas(fit$draws(c("mu"))) #locations
-mcmc_areas(fit$draws(c("sigma"))) #scales
+mcmc_areas(fit$draws(c("zeta"))) #locations
+mcmc_areas(fit$draws(c("omega"))) #scales
 mcmc_areas(fit$draws(c("alpha"))) #skew
 
 
@@ -97,7 +108,6 @@ get_player_pars = function(fit, player_pars, global_pars) {
   } else {
     player_pars_sum
   }
-  
 }
 
 #function that takes in fitted pars and outputs df with true values and predicted values
@@ -125,7 +135,7 @@ get_rmse = function(results) {
 }
 
 #fitted pars from baseline fit
-baseline_fitted_pars_2024 = get_player_pars(fit, c("mu"), c("sigma", "alpha"))
+baseline_fitted_pars_2024 = get_player_pars(fit, c("zeta"), c("omega", "alpha"))
 
 #results (true vals vs predicted vals)
 baseline_results = get_results(baseline_fitted_pars_2024, true_vals)
@@ -144,6 +154,7 @@ baseline_rmse
 
 
 
+
 ### Advanced Model ### 
 
 
@@ -155,11 +166,23 @@ baseline_rmse
 #' 1. add player-specific scales (sigma)
 #'    - improvement 1.59 --> 1.44 rmse
 #' 
-#' 2. adding hierarchical structure
+#' 2. adding hierarchical structure to zeta, omega
 #'    - improvement 1.44 --> 1.41 rmse
-#'    - increases computation time by wuite a bit though
+#'    - increases computation time by quite a bit though
 #' 
 #' 3. adding player-specific skew
+#'    - no improvement 1.41 --> 1.42 rmse
+#'    - also increases model complexity and computation time
+#'    - don't adopt
+#' 
+#' 4. adding global hieght effect
+#'    - no improvement 1.41 --> 1.71
+#'    - this should be an improvement so need to look into it
+#'    - heights were centered and scaled
+#'    
+#' 5. adding global weight effect
+#'
+#' 
 #' 
 
 
@@ -172,6 +195,7 @@ mod = cmdstan_model(stan_file)
 
 #sample from model
 fit = mod$sample(data = stan_data,
+                 #init = 0,
                  seed = 123,
                  chains = 4,
                  parallel_chains = 4,
@@ -184,15 +208,17 @@ fit = mod$sample(data = stan_data,
 #read in fit
 #fit = readRDS(file = here("stan fits", "advanced.RDS"))
 
-fit$summary() 
+fit$summary() %>% print(n = 30)
 #good convergence - rhat close to 1
 
 #posteriors
 mcmc_areas(fit$draws(c("zeta"))) #locations
+mcmc_areas(fit$draws(c("delta"))) #height effect
 mcmc_areas(fit$draws(c("omega"))) #scales
-mcmc_areas(fit$draws(c("mu_zeta"))) #locations
-mcmc_areas(fit$draws(c("mu_omega"))) #scales
 mcmc_areas(fit$draws(c("alpha"))) #skew
+mcmc_areas(fit$draws(c("mu_zeta"))) #locations global mean
+mcmc_areas(fit$draws(c("mu_omega"))) #scales global meanff
+mcmc_areas(fit$draws(c("sigma_zeta", "sigma_omega"))) #location, scale, skew global variance
 
 #get fitted pars
 advanced_fitted_pars_2024 = get_player_pars(fit, player_pars = c("zeta", "omega"), global_pars = "alpha")
